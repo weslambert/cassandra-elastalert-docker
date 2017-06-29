0ae90b06853715bc5a1dd024af328a58383d156a
weslambert <wlambertts@gmail.com>
2017-06-29 16:12:01 -0400

    remove unnecessary lines

--- files/start-elastalert.sh
+++ files/start-elastalert.sh
@@ -36,7 +36,6 @@ else
 fi
 rm -f garbage_file
 
-
 # Elastalert configuration:
 # Set the rule directory in the Elastalert config file to external rules directory.
 sed -i -e"s|^rules_folder: [[:print:]]*|rules_folder: ${RULES_DIRECTORY}|g" ${ELASTALERT_CONFIG}; \
@@ -56,32 +55,6 @@ sed -i -e"s|stderr_logfile=.*log|stderr_logfile=${LOG_DIR}/elastalert_stderr.log
 
 # Modify the start-command.
 sed -i -e"s|python elastalert.py|python -m elastalert.elastalert --config ${ELASTALERT_CONFIG}|g" ${ELASTALERT_SUPERVISOR_CONF}; \
-
-# Elastalert IDS rule configuration:
-# Set the Elasticsearch host that Elastalert is to query.
-sed -i -e"s|^es_host: [[:print:]]*|es_host: ${ELASTICSEARCH_HOST}|g" ${RULES_DIRECTORY}/ids.yaml; \
-
-# Set the port used by Elasticsearch at the above address.
-sed -i -e"s|^es_port: [0-9]*|es_port: ${ELASTICSEARCH_PORT}|g" ${RULES_DIRECTORY}/ids.yaml; \
-
-# Set the index name by Elasticsearch.
-#sed -i -e"s|^index: [[:print:]]*|index: ${ELASTICSEARCH_CASSANDRA_INDEX}|g" ${RULES_DIRECTORY}/ids.yaml; \
-
-# Set the slack webhook url.
-#sed -i -e"s|^slack_webhook_url: [[:print:]]*|slack_webhook_url: ${SLACK_WEBHOOK_URL}|g" ${RULES_DIRECTORY}/ids.yaml; \
-
-# Elastalert BRO_CONN rule configuration:
-# Set the Elasticsearch host that Elastalert is to query.
-sed -i -e"s|^es_host: [[:print:]]*|es_host: ${ELASTICSEARCH_HOST}|g" ${RULES_DIRECTORY}/bro_conn.yaml; \
-
-# Set the port used by Elasticsearch at the above address.
-sed -i -e"s|^es_port: [0-9]*|es_port: ${ELASTICSEARCH_PORT}|g" ${RULES_DIRECTORY}/bro_conn.yaml; \
-
-# Set the index name by Elasticsearch.
-#sed -i -e"s|^index: [[:print:]]*|index: ${ELASTICSEARCH_CASSANDRA_INDEX}|g" ${RULES_DIRECTORY}/bro_conn.yaml; \
-
-# Set the slack webhook url.
-#sed -i -e"s|^slack_webhook_url: [[:print:]]*|slack_webhook_url: ${SLACK_WEBHOOK_URL}|g" ${RULES_DIRECTORY}/bro_conn.yaml; \
 
 echo "Starting Elastalert..."
 exec supervisord -c ${ELASTALERT_SUPERVISOR_CONF} -n
